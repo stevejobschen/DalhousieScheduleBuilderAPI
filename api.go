de93480ae5e7ddce0818fea04e99c5fb2a2bd2a3
Steve <zzchenyufei@gmail.com>
2019-01-08 18:32:05 -0400

    Update api.go

--- api.go
+++ api.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-  "net/http"
-  "fmt"
-  "dal-api/db"
-  "time"
+	"fmt"
+	"net/http"
+	"time"
+
+	"./db"
 )
 
 func main() {
-  fmt.Printf("API has started running on port: 8080... \n")
-  http.HandleFunc("/api/courses", courses)
-  http.ListenAndServe(":8080", nil)
+	fmt.Printf("API has started running on port: 8080... \n")
+	http.HandleFunc("/api/courses", courses)
+	http.ListenAndServe(":8080", nil)
 }
 
 func courses(w http.ResponseWriter, r *http.Request) {
-  t := time.Now()
+	t := time.Now()
 	tn := t.Format("2006/01/02 15:04:05")
-  fmt.Printf("[" + tn + "] New connection made")
-  w.Header().Set("Access-Control-Allow-Origin", "*")
-  w.Header().Set("Content-Type", "application/json")
-  category := r.URL.Query().Get("s")
-  term := r.URL.Query().Get("t")
-  fmt.Fprintf(w, string(db.Open(category, term)))
+	fmt.Printf("[" + tn + "] New connection made")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	category := r.URL.Query().Get("s")
+	term := r.URL.Query().Get("t")
+	fmt.Fprintf(w, string(db.Open(category, term)))
 }
