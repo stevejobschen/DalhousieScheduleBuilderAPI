92923080b787cd332028ec59054d98c27cb31e12
Steve <zzchenyufei@gmail.com>
2019-04-08 23:37:43 -0300

    It works!
    God know how it work.
    
    //rewrite the comment connect database and it works

--- db/database.go
+++ db/database.go
@@ -1,65 +1,66 @@
 package db
 
 import (
-  _ "github.com/go-sql-driver/mysql"
-  "database/sql"
-  "encoding/json"
-  "strconv"
+	"database/sql"
+	"encoding/json"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Class struct {
-  Id int `json:"id"`
-  Crn int `json:"crn"`
-  Section string `json:"section"`
-  Type string `json:"type"`
-  CreditHours float32 `json:"credit_hours"`
-  Days string `json:"days"` // maybe make this an array
-  Times string `json:"times"` // make this a different data type easier to work with
-  Location string `json:"location"`
-  Max string `json:"max"` // make this int array
-  Current string `json:"current"` // make this int array
-  Waitlist string `json:"waitlist"`
-  Prof string `json:"prof"`
+	Id          int     `json:"id"`
+	Crn         int     `json:"crn"`
+	Section     string  `json:"section"`
+	Type        string  `json:"type"`
+	CreditHours float32 `json:"credit_hours"`
+	Days        string  `json:"days"`  // maybe make this an array
+	Times       string  `json:"times"` // make this a different data type easier to work with
+	Location    string  `json:"location"`
+	Max         string  `json:"max"`     // make this int array
+	Current     string  `json:"current"` // make this int array
+	Waitlist    string  `json:"waitlist"`
+	Prof        string  `json:"prof"`
 }
 
 type Course struct {
-  Category string `json:"category"`
-  Code string `json:"code"`
-  Title string `json:"title"`
-  Classes []Class  `json:"classes"`
+	Category string  `json:"category"`
+	Code     string  `json:"code"`
+	Title    string  `json:"title"`
+	Classes  []Class `json:"classes"`
 }
 
 func Open(code string, term string) []byte {
-  db, _ := sql.Open("mysql", "root@/dal")
+	db, _ := sql.Open("mysql", "root:lalaland123@/dal") //("mysql", "root@/dal")
 
-  // seeing if I can get a working json return
-  var courseQuery string = "SELECT id, category, code, title FROM courses WHERE category='" + code + "' and term=" + term + ";"
-  rows, _ := db.Query(courseQuery)
+	// seeing if I can get a working json return
+	var courseQuery string = "SELECT id, category, code, title FROM courses WHERE category='" + code + "' and term=" + term + ";"
+	rows, _ := db.Query(courseQuery)
 
-  var storage struct {
-    Data []Course `json:"data"`
-  }
+	var storage struct {
+		Data []Course `json:"data"`
+	}
 
-  defer rows.Close()
-  for rows.Next() {
+	defer rows.Close()
+	for rows.Next() {
 
-    var c Course
-    var cid int
-    rows.Scan(&cid, &c.Category, &c.Code, &c.Title)
+		var c Course
+		var cid int
+		rows.Scan(&cid, &c.Category, &c.Code, &c.Title)
 
-    var classQuery string = "SELECT classes.* FROM classes INNER JOIN course_classes ON course_classes.class=classes.id INNER JOIN courses ON course_classes.course=courses.id where course_classes.course=" + strconv.Itoa(cid) + ";"
+		var classQuery string = "SELECT classes.* FROM classes INNER JOIN course_classes ON course_classes.class=classes.id INNER JOIN courses ON course_classes.course=courses.id where course_classes.course=" + strconv.Itoa(cid) + ";"
 
-    classRows, _ := db.Query(classQuery)
-    for classRows.Next() {
-      var cl Class
-      classRows.Scan(&cl.Id, &cl.Crn, &cl.Section, &cl.Type, &cl.CreditHours, &cl.Days, &cl.Times, &cl.Location, &cl.Max, &cl.Current, &cl.Waitlist, &cl.Prof)
-      c.Classes = append(c.Classes, cl)
-    }
-    storage.Data = append(storage.Data, c)
-  }
+		classRows, _ := db.Query(classQuery)
+		for classRows.Next() {
+			var cl Class
+			classRows.Scan(&cl.Id, &cl.Crn, &cl.Section, &cl.Type, &cl.CreditHours, &cl.Days, &cl.Times, &cl.Location, &cl.Max, &cl.Current, &cl.Waitlist, &cl.Prof)
+			c.Classes = append(c.Classes, cl)
+		}
+		storage.Data = append(storage.Data, c)
+	}
 
-  p, _ := json.Marshal(storage)
+	p, _ := json.Marshal(storage)
 
-  return p;
+	return p
 
 }
